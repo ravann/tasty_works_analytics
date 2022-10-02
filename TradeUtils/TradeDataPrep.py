dc7c7fb8cfa88d393c93392d2376c679f49b3e2f
Ravan Nannapaneni <ravan_n@hotmail.com>
2022-10-02 14:16:47 +0800

    Moved the functions ...

--- TradeUtils/TradeDataPrep.py
+++ TradeUtils/TradeDataPrep.py
@@ -11,6 +11,9 @@ class TradeDataPrep:
         self._convert_dates()
         self.hist_df = hist_df.sort_values(by=["date_time"])
         self._convert_floats()
+        self._fix_missing_data()
+        self._set_open_close()
+        self._set_buy_sell()
 
     def _convert_dates(self) -> None:
         self.hist_df["expiration_date"] = pd.to_datetime(self.hist_df.expiration_date, format='%m/%d/%y', errors='coerce')
@@ -20,35 +23,32 @@ class TradeDataPrep:
         self.hist_df["commission"] = self.hist_df.apply(lambda x: float(x.commissions.replace("--", "0")), axis=1 )
         self.hist_df["value"] = self.hist_df.apply(lambda x: float(x.value.replace(",", "")), axis=1 )
 
+    def _fix_missing_data(self) -> None: 
+        # symbol
+        self.hist_df["root_symbol"] = self.hist_df.apply(lambda x: x.root_symbol if (pd.notnull(x.root_symbol)) else x.symbol, axis = 1 )
+        self.hist_df["underlying_symbol"] = self.hist_df.apply(lambda x: x.underlying_symbol if (pd.notnull(x.underlying_symbol)) else x.symbol, axis = 1 )
+
+        # expiration_date, strike_price, call_or_put
+        self.hist_df["expiration_date"] = self.hist_df.apply(lambda x: "12/31/99" if (x.instrument_type == "Equity") else x.expiration_date, axis = 1 )
+        self.hist_df["strike_price"] = self.hist_df.apply(lambda x: "0" if (x.instrument_type == "Equity") else x.strike_price, axis = 1 )
+        self.hist_df["call_or_put"] = self.hist_df.apply(lambda x: "NA" if (x.instrument_type == "Equity") else x.call_or_put, axis = 1 )
+
+    def _set_open_close(self):
+        self.hist_df["open_close"] = self.hist_df.apply(lambda x: "Open" if ( x.action in ["BUY_TO_OPEN", "SELL_TO_OPEN"] ) else "Close", axis = 1 )
+
+    def _set_buy_sell(self):
+        self.hist_df["buy_sell"] = self.hist_df.apply(lambda x: "Buy" if ( x.action in ["BUY_TO_OPEN", "BUY_TO_CLOSE"] ) else "Sell", axis = 1 )
+        self.hist_df.to_csv("buy_sell.csv", index=False)
+
+    def get_prepared_data(self): 
+        return self.hist_df
 
     ### PREPARE TXN DATASET ###
 
     def get_txn_df(self) -> pandas.DataFrame:
         self._filter_trades()
-        self._fix_missing_data()
-        self._set_open_close()
-        self._set_buy_sell()
         return self.txn_df
 
     def _filter_trades(self) -> None:
         self.txn_df: pandas.DataFrame = self.hist_df[self.hist_df.type.isin(['Trade', 'Receive Deliver'])]
-
-    def _fix_missing_data(self) -> None: 
-        # symbol
-        self.txn_df["root_symbol"] = self.txn_df.apply(lambda x: x.root_symbol if (pd.notnull(x.root_symbol)) else x.symbol, axis = 1 )
-        self.txn_df["underlying_symbol"] = self.txn_df.apply(lambda x: x.underlying_symbol if (pd.notnull(x.underlying_symbol)) else x.symbol, axis = 1 )
-
-        # expiration_date, strike_price, call_or_put
-        self.txn_df["expiration_date"] = self.txn_df.apply(lambda x: "12/31/99" if (x.instrument_type == "Equity") else x.expiration_date, axis = 1 )
-        self.txn_df["strike_price"] = self.txn_df.apply(lambda x: "0" if (x.instrument_type == "Equity") else x.strike_price, axis = 1 )
-        self.txn_df["call_or_put"] = self.txn_df.apply(lambda x: "NA" if (x.instrument_type == "Equity") else x.call_or_put, axis = 1 )
-
-
-    def _set_open_close(self):
-        self.txn_df["open_close"] = self.txn_df.apply(lambda x: "Open" if ( x.action in ["BUY_TO_OPEN", "SELL_TO_OPEN"] ) else "Close", axis = 1 )
-        # print(self.txn_df[self.txn_df.type == "Receive Deliver"].head(2).T)
-
-    def _set_buy_sell(self):
-        self.txn_df["buy_sell"] = self.txn_df.apply(lambda x: "Buy" if ( x.action in ["BUY_TO_OPEN", "BUY_TO_CLOSE"] ) else "Sell", axis = 1 )
-        self.txn_df.to_csv("buy_sell.csv", index=False)
 
